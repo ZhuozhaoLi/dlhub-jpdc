27160138e64954f0af2a0d4e575ba6175090c9fe
Zhuozhao Li <zhuozhl@clemson.edu>
2019-10-16 21:38:37 +0000

    minor changes

--- theta-scaling/run.sh
+++ theta-scaling/run.sh
@@ -8,9 +8,9 @@ tasks=5000
 endpoint_name="dlhub-theta-remote"
 queue='default'
 #queue='debug-flat-quad'
-for i in 1 2 4 8 16 32 64 128 256 512;
+for c in "noop" "oqmd" "matminer-featurize" "matminer-util" "cifar10" "inception";
 do
-    for c in "noop" "oqmd" "matminer-featurize" "matminer-util" "cifar10" "inception";
+    for i in 1 2 4 8 16 32 64 128 256 512;
     do
         echo "Running with $i $c containers"
         python run.py -i $i -n $trials -c $c -a $max_workers_per_node -t $tasks -w $walltime -y $endpoint_name
